90d8bea2855549d2920dda4f2fb1c664edd91014
RenanMaestrya <renanalmeida2.ra@gmail.com>
2023-10-27 16:41:54 -0300

    finisha

---
+++ app/core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', ListStudent.as_view(), name='list_student'),
+    path('create_student/', CreateStudent.as_view(), name='create_student'),
+    path('edit_student/<int:pk>/', EditStudent.as_view(), name='edit_student'),
+    path('delete_student/<int:pk>/', DeleteStudent.as_view(), name='delete_student'),
+    path('student_detail/<int:pk>/', StudentDetail.as_view(), name='student_detail'),
+]
